335cd2841e0063b5d17e16cd53445f6554f10b11
JeremySkoler <jskoler@olin.edu>
2020-04-27 16:55:57 -0400

    DP working

--- discreteNoah.py
+++ discreteNoah.py
@@ -1,6 +1,7 @@
 import math
 import random
 import numpy as np
+import matplotlib.pyplot as plt
 
 class Animal:
     ''' Creates an animal'''
@@ -13,11 +14,12 @@ class Animal:
         '''
 
         self.name = name
-        self.survivial_params = self.survivial_params # [0.5, 2, 0.2]
-        self.value_constant = self.value_constant
+        self.survivial_params = survival_params # [0.5 (0-1), 2, 0.2 (starting survival probability, between 0-1)]
+        self.value_constant = value_constant
+        self.resources = 0
 
-    def get_value(self, money):
-        val = self.value_constant*self.survivial_params[0]*sqrt(money/self.survivial_params[1])+self.survivial_params[2]
+    def get_value(self, resource):
+        val = self.value_constant*(self.survivial_params[0]*math.sqrt(resource/self.survivial_params[1])+self.survivial_params[2])
         if val <=1:
             return val
         else:
@@ -25,7 +27,7 @@ class Animal:
 
 
 
-def knapsack(animalList, resources):
+def resource_allocation(animalList, resources):
     ''' Find optimal subset of animals to maximize the total value while remaining
      within the resources limit
 
@@ -40,38 +42,61 @@ def knapsack(animalList, resources):
     dp[resources][item] (float) = The maximized value achieved by optimal subset
     '''
     anm_count = len(animalList)
-    dp = np.zeros((resources+1, anm_count+1))          #Matrix to hold subset values
+    dp = np.zeros((resources+1, anm_count))          #Matrix to hold subset values
 
     totalValue = 0
     for animal in animalList:
         totalValue += animal.get_value(0)
 
-    for i in range(anm_count+1):
+    print(totalValue)
+
+    for j in range(anm_count):
         dp[0][j] = totalValue
 
+    where_dat_resource = 0
 
-    for i in range(anm_count+1):                   #Each animal option
-        for j in range(resources+1):               #Each potential 'remaining' resources
-                                            #Take max of best value with/without animal
-            dp[i,j] = max(dp[i,j-1], animalList[i].value + dp[i-animalList[j].weight, j-1])
+    for i in range(1,resources+1):               #Each potential 'remaining' resources
+        for j in range(anm_count):
+            a = animalList[j]               #Each animal option
+            if j == 0:
+                ## For first column, otherwise index out of range
+                dp[i][j] = dp[i-1][anm_count-1] + (a.get_value(a.resources+1)-a.get_value(a.resources))
+            else:
+                ## Take max of totalValue with/without resource for this animal
+                dp[i][j] = max(dp[i][j-1], dp[i-1][anm_count-1] + (a.get_value(a.resources+1)-a.get_value(a.resources)))
+                if dp[i][j] != dp[i][j-1]:
+                    where_dat_resource = j
+
+        ## This keeps track of where the resource was assigned
+        animalList[where_dat_resource].resources += 1
 
 
     ## This works through the final dp matrix looking for animals that are in
     ## the ideal subset and adding their names to a list
-    animalsIncluded = []
-    aniTracker = items              #Track i (column) position in table
-    weightTracker = weight          #Tracks w (row) position in table
-    while aniTracker > 0:
-        ## If the value is greater than the previous value without the current
-        ## animal it means that the current animal was used in the final solution
-        if dp[weightTracker][aniTracker] > dp[weightTracker][aniTracker-1]:
-            currentAnimal = animalList[aniTracker-1]
-            animalsIncluded.append(currentAnimal.name)
-            weightTracker -= currentAnimal.weight     #Account for added animal's weight
-        aniTracker -= 1
+    # animalsIncluded = []
+    # aniTracker = items              #Track i (column) position in table
+    # weightTracker = weight          #Tracks w (row) position in table
+    # while aniTracker > 0:
+    #     ## If the value is greater than the previous value without the current
+    #     ## animal it means that the current animal was used in the final solution
+    #     if dp[weightTracker][aniTracker] > dp[weightTracker][aniTracker-1]:
+    #         currentAnimal = animalList[aniTracker-1]
+    #         animalsIncluded.append(currentAnimal.name)
+    #         weightTracker -= currentAnimal.weight     #Account for added animal's weight
+    #     aniTracker -= 1
+    res = []
+    names = []
+    for animal in animalList:
+        names.append(animal.name)
+        res.append(animal.resources)
 
 
-    return (animalsIncluded, dp[weight][items])
+
+    plt.bar(range(len(animalList)), res, tick_label = names)
+    plt.show()
+
+
+    return (dp[resources][anm_count-1])
 
 
 # Arbitrary animals
@@ -83,4 +108,5 @@ horse = Animal('horse', [1, 2, 0.3], 5)
 beardedDragon = Animal('bearded dragon', [0.5, 1, 0.4], 6)
 
 testList = [cow, cat, mouse, elephant, horse, beardedDragon]
-print(knapsack(testList, 7))            #Arbitrary weight
+
+print(resource_allocation(testList,7))            #Arbitrary weight
