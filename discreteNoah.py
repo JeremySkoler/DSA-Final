26ee6ef5daeb6e00e1b59d7c9c40ef941d795308
JeremySkoler <jskoler@olin.edu>
2020-04-28 11:06:33 -0400

    Test optimality

--- discreteNoah.py
+++ discreteNoah.py
@@ -18,12 +18,18 @@ class Animal:
         self.value_constant = value_constant
         self.resources = 0
 
-    def get_value(self, resource):
-        val = self.value_constant*(self.survivial_params[0]*math.sqrt(resource/self.survivial_params[1])+self.survivial_params[2])
-        if val <=1:
-            return val
-        else:
-            return 1
+    def get_survival_rate(self, resources):
+        survival_chance = (self.survivial_params[0]*math.sqrt(resources/self.survivial_params[1])+self.survivial_params[2])
+        return survival_chance
+
+    def get_value(self, resources):
+        survival_chance = self.get_survival_rate(resources)
+        if survival_chance >=1:
+            survival_chance = 1
+        #val = self.value_constant*(1+0.3*survival_chance)
+        val = self.value_constant*survival_chance
+        return val
+
 
 
 
@@ -48,66 +54,68 @@ def resource_allocation(animalList, resources):
     for animal in animalList:
         totalValue += animal.get_value(0)
 
-    print(totalValue)
 
     for j in range(anm_count):
         dp[0][j] = totalValue
 
     where_dat_resource = 0
 
-    for i in range(1,resources+1):               #Each potential 'remaining' resources
-        for j in range(anm_count):
+    for i in range(1,resources+1):
+        a = animalList[0]
+        dp[i][0] = dp[i-1][anm_count-1] + (a.get_value(a.resources+1)-a.get_value(a.resources))
+            #Each potential 'remaining' resources
+        for j in range(1, anm_count):
             a = animalList[j]               #Each animal option
-            if j == 0:
-                ## For first column, otherwise index out of range
-                dp[i][j] = dp[i-1][anm_count-1] + (a.get_value(a.resources+1)-a.get_value(a.resources))
-            else:
-                ## Take max of totalValue with/without resource for this animal
-                dp[i][j] = max(dp[i][j-1], dp[i-1][anm_count-1] + (a.get_value(a.resources+1)-a.get_value(a.resources)))
-                if dp[i][j] != dp[i][j-1]:
-                    where_dat_resource = j
+            ## Take max of totalValue with/without resource for this animal
+            dp[i][j] = max(dp[i][j-1], dp[i-1][anm_count-1] + (a.get_value(a.resources+1)-a.get_value(a.resources)))
+
+            if dp[i][j] != dp[i][j-1]:
+                where_dat_resource = j
+
 
         ## This keeps track of where the resource was assigned
         animalList[where_dat_resource].resources += 1
 
 
-    ## This works through the final dp matrix looking for animals that are in
-    ## the ideal subset and adding their names to a list
-    # animalsIncluded = []
-    # aniTracker = items              #Track i (column) position in table
-    # weightTracker = weight          #Tracks w (row) position in table
-    # while aniTracker > 0:
-    #     ## If the value is greater than the previous value without the current
-    #     ## animal it means that the current animal was used in the final solution
-    #     if dp[weightTracker][aniTracker] > dp[weightTracker][aniTracker-1]:
-    #         currentAnimal = animalList[aniTracker-1]
-    #         animalsIncluded.append(currentAnimal.name)
-    #         weightTracker -= currentAnimal.weight     #Account for added animal's weight
-    #     aniTracker -= 1
-
-
     res = []
     names = []
     for animal in animalList:
-        names.append(animal.name)
+        names.append("%s: %.2f" % (animal.name, animal.get_value(animal.resources)))
         res.append(animal.resources)
+
 
     plt.bar(range(len(animalList)), res, tick_label = names)
     plt.show()
 
+    return res
 
 
-    return (dp[resources][anm_count-1])
-
+def test_optimality(animals, res, n):
+    """ does several random simulations of which animals live and die
+    animals = list of animals objects
+    res = result list of money allocated
+    n = number of random iterations
+    """
+    value = 0
+    for i in range(len(animals)):
+        a = animals[i]
+        prob = a.get_survival_rate(res[i]) # find probability each animal will survive
+        # print(prob)
+        for j in range(n):
+            animal_survives = random.random() < prob # find out if animal survivies (random)
+            if animal_survives:
+                #print(a.name)
+                value += a.value_constant # add animal's value to total value
+    return value/n
 
 # Arbitrary animals
-cow = Animal ('cow', [0.5, 2, 0.5], 4)
-cat = Animal ('cat', [0.1, 1, 0.7], 1)
-mouse = Animal ('mouse', [0.3, 2, 0.4], 3)
-elephant = Animal ('elephant', [0.5, 3, 0.1], 8)
-horse = Animal('horse', [1, 2, 0.3], 5)
-beardedDragon = Animal('bearded dragon', [0.5, 1, 0.4], 6)
+cow = Animal ('cow', [0.5, 20, 0.5], 8)
+cat = Animal ('cat', [0.1, 10, 0.7], 5)
+mouse = Animal ('mouse', [0.3, 20, 0.4], 7)
+elephant = Animal ('elephant', [0.5, 30, 0.1], 4)
+horse = Animal('horse', [1, 20, 0.3], 2)
+beardedDragon = Animal('bDragon', [0.5, 10, 0.4], 2)
 
 testList = [cow, cat, mouse, elephant, horse, beardedDragon]
 
-print(resource_allocation(testList,7))            #Arbitrary weight
+print(test_optimality(testList, resource_allocation(testList,90), 2))
